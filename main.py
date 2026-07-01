f1083dd4b7b4aee0d75df35e5cea4bb485c38c53
tuxarith <py.and.tg.developer@proton.me>
2026-07-01 09:27:21 +0300

    Add Telegram weather bot

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
-import dotenv
+
 from os import getenv
 from aiogram import Dispatcher, Bot
 from dotenv import load_dotenv
-from aiogram.client.session.aiohttp import AiohttpSession
+
 from router import router
 load_dotenv("bot_token.env")
 
@@ -13,11 +13,11 @@ dp = Dispatcher()
 
 dp.include_router(router)
 
-session = AiohttpSession(proxy='socks5://oo1gyn:JTsRyT@45.145.57.210:11623')
+
 
 async def main():
 
- bot = Bot(token=Token, session=session)
+ bot = Bot(token=Token)
 
  print("started")
  print("""───▐▀▄──────▄▀▌───▄▄▄▄▄▄▄
